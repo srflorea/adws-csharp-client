e8cb86145fedc1a16732423c88a363e32fa16739
Razvan Florea <razvan.florea91@gmail.com>
2014-05-16 15:33:15 +0200

    Created VS project

---
+++ Adws CSharp Client/Adws CSharp Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adws_CSharp_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
